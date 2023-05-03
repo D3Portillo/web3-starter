ddb71531e6f8ebbab5044f2cc13c09ee974d6eb3
D3Portillo <d3portillo@gmail.com>
2023-05-03 15:15:55 +0200

    chores: add prettier config and action

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type { import("prettier").Config } */
+
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  printWidth: 80,
+  jsxSingleQuote: false,
+  arrowParens: "always",
+  useTabs: false,
+  endOfLine: "lf",
+}
